2db9f835a22a3180a7c46959dc1c66ec9013057d
Reymo Je <rubicsoft0@gmail.com>
2025-05-12 09:42:15 +0700

    Initial commit :)

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prepare for OSN-K 2025
---
+++ Source/Starter/main.cpp
@@ -0,0 +1,14 @@
+/*
+    First starter code by Reymo Je
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "Hello, You" << endl;
+
+    return 0;
+}
