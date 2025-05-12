11da388c8baffa3b0f11bf3a4c556b6af9710a21
Reymo Je <rubicsoft0@gmail.com>
2025-05-12 11:11:19 +0700

    Iteration function on recursiveFunction for performance test There is some misvalue occurs on the iteration function

--- Source/Recursion/recursiveFunction.cpp
+++ Source/Recursion/recursiveFunction.cpp
@@ -1,24 +1,35 @@
 /*
     RECURSIVE FUNCTION
+    Date : May 12, 2025
 */
 
 #include <iostream>
 
+
+#define RECURSIVE 0
+#define ITERATION 1
+
+// CONFIG
 #define FACTORIAL_CONSTANT 10
+#define FACTORIAL_METHOD ITERATION
 
 
 unsigned long recursiveFactorial(unsigned int n);
-// unsigned long iterationFactorial(unsigned int n);
+unsigned long iterationFactorial(unsigned int n);
 
 
 
 int main() {
     long factorialResult;
 
+    #if FACTORIAL_METHOD == RECURSIVE
     factorialResult = recursiveFactorial(FACTORIAL_CONSTANT);
+    #elif FACTORIAL_METHOD == ITERATION
+    factorialResult = iterationFactorial(FACTORIAL_CONSTANT);
+    #endif
 
 
-    std::cout << "RESULT : " << factorialResult << std::endl;
+    std::cout << "FACTORIAL RESULT : " << factorialResult << std::endl;
 
     return 0;
 }
@@ -35,13 +46,18 @@ unsigned long recursiveFactorial(unsigned int n) {
     return n * recursiveFactorial(n - 1);
 }
 
-// Factorial function using iteration
-// unsigned long iterationFactorial(unsigned int n) {
-//     unsigned long result;
-
-//     while (result )
-//     {
-//         /* code */
-//     }
+// Factorial function using iteration (Code from Deepseek)
+// Please don't expect much :v
+unsigned long iterationFactorial(unsigned int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
     
-// }
+    unsigned long a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
